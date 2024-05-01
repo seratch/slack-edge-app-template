e4ce3005cc432b564091b86eda8182fe26870dec
Kazuhiro Sera <seratch@gmail.com>
2024-05-01 09:50:40 +0900

    Upgrade socket-mode client to v2

--- src/main.ts
+++ src/main.ts
@@ -6,24 +6,20 @@ import {
 } from "slack-edge";
 import { app } from "./app";
 
-const socketModeClient = new SocketModeClient({
+const sm = new SocketModeClient({
   appToken: process.env.SLACK_APP_TOKEN!,
   logLevel: (process.env.SLACK_APP_LOG_LEVEL as LogLevel) || LogLevel.DEBUG,
 });
 
 // No need to modify the following lines of code
-socketModeClient.on(
+sm.on(
   "slack_event",
   async ({ body, ack, retry_num: retryNum, retry_reason: retryReason }) => {
     const request = fromSocketModeToRequest({ body, retryNum, retryReason });
-    if (!request) {
-      return;
-    }
+    if (!request) return;
     const response = await app.run(request);
     await ack(await fromResponseToSocketModePayload({ response }));
   },
 );
 
-(async () => {
-  await socketModeClient.start();
-})();
+(async () => await sm.start())();
